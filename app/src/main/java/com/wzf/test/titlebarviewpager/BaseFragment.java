72ac805a7865cebc2a78fd3d7dec8e98cda5c3b0
zhenfei.wang <zhenfei.wang>
2016-03-18 15:40:06 +0800

    first commit

---
+++ app/src/main/java/com/wzf/test/titlebarviewpager/BaseFragment.java
@@ -0,0 +1,20 @@
+package com.wzf.test.titlebarviewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/3/18.
+ */
+public class BaseFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("测试" + System.currentTimeMillis());
+        return textView;
+    }
+}
